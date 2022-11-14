485259bc59358841e9344930ff82895dfbb2469c
Sebastien Bonnet <49565350+sbonnet-dev@users.noreply.github.com>
2022-11-14 21:45:59 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "azurerm_public_ip" "public_ip" {
+  name = lower(format("%s%s%s", var.rg_type, var.environment, var.name))
+
+  location            = var.location
+  resource_group_name = var.rg_name
+
+  allocation_method   = "Static"
+
+  tags = {
+    project     = var.project
+    environment = var.environment
+    owner       = var.owner
+  }
+}
